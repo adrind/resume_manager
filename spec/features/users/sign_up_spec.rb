6806b48d6db04d55ecfda17c249e26c5ac938c78
Adrienne <adrienne@codeforamerica.org>
2017-05-25 10:32:30 -0700

    added integration test for sign up flow

---
+++ spec/features/users/sign_up_spec.rb
@@ -0,0 +1,29 @@
+# Feature: Sign out
+#   As a user
+#   I want to sign out
+#   So I can protect my account from unauthorized access
+feature 'Sign up', :omniauth do
+
+  # Scenario: User signs out successfully
+  #   Given I am signed in
+  #   When I sign out
+  #   Then I see a signed out message
+  scenario 'user signs up successfully' do
+    signin
+    expect(page).to have_content 'Users: 1 registered'
+    click_link 'Users'
+    click_link 'Adrienne'
+    page.has_selector?('form')
+    expect(form_field('name')).to match 'Adrienne'
+
+    expect(form_ed_field('name')).to match 'Ivy Puppiez'
+    expect(form_ed_field('level')).to match 'Puppy academy'
+    expect(form_ed_field('year')).to match '2017'
+
+    expect(form_job_field('position')).to match 'Puppy cuddler'
+    expect(form_job_field('employer')).to match 'SPCA'
+    expect(form_job_field('start_date')).to match '2017-02-01'
+
+  end
+
+end
--- spec/support/helpers/omniauth.rb
+++ spec/support/helpers/omniauth.rb
@@ -6,8 +6,38 @@ module Omniauth
         'provider' => 'facebook',
         'uid' => '123545',
         'info' => {
-          'name' => 'mockuser'
+          'name' => 'Adrienne'
         },
+        'extra' => {
+            'raw_info' => {
+              'work' => [
+                  {
+                      'position' => {
+                          'name' => 'Puppy cuddler'
+                      },
+                      'employer' => {
+                          'name' => 'SPCA'
+                      },
+                      'location' => {
+                          'name' => 'Where the puppies are'
+                      },
+                      'start_date' => '2017-02-01',
+                      'description' => 'Cuddling puppies day n nite'
+                  }
+              ],
+              'education' => [
+                  {
+                      'school' => {
+                          'name' => 'Ivy Puppiez'
+                      },
+                      'type' => 'Puppy academy',
+                      'year' => {
+                          'name' => '2017'
+                      }
+                  }
+              ]
+            }
+          },
         'credentials' => {
           'token' => 'mock_token',
           'secret' => 'mock_secret'
@@ -22,6 +52,18 @@ module Omniauth
       expect(page).to have_content("Sign in plz")
       auth_mock
       click_link "Here"
+    end
+
+    def form_field(key)
+      find_field(id: "user_#{key}").value
+    end
+
+    def form_ed_field(key)
+      find_field(id: "user_educations_attributes_0_#{key}").value
+    end
+
+    def form_job_field(key)
+      find_field(id: "user_jobs_attributes_0_#{key}").value
     end
   end
 
